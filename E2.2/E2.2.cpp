f4e83dcdcadcb1af1a51113eed346853a517f690
Evelyn-nn <822812694@qq.com>
2017-02-08 16:51:48 +0800

    C++课后习题，要求用户输入一个以long为单位的距离，然后将它转化为码。

---
+++ E2.2/E2.2.cpp
@@ -0,0 +1,18 @@
+// E2.2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int l;
+	int n;
+	cout << "Please write down a distans: ";
+	cin >> l;
+	n = 220 * l;
+	cout << l <<"long = " << n << "ma"<<endl;
+    return 0;
+}
+
